c4a9d70151edbcc4c187fb2b3e1659a28c7067bc
kangkong <kang3530554@gmail.com>
2023-06-26 11:47:43 +0900

    💄 Learn : 모바일 환경에서 layout 배치 방법 학습

--- App.js
+++ App.js
@@ -1,20 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1 }}>
+      <View style={{ flex: 1, backgroundColor: "tomato" }} />{" "}
+      {/* 부모 요소 flex 크기의 1배 */}
+      <View style={{ flex: 2, backgroundColor: "teal" }} />{" "}
+      {/* 부모 요소 flex 크기의 2배 */}
+      <View style={{ flex: 3, backgroundColor: "orange" }} />{" "}
+      {/* 부모 요소 flex 크기의 3배 */}
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
