e7bc43d22388a649a6ee75f3448009d3595bd7d6
kangkong <kang3530554@gmail.com>
2023-06-26 17:17:44 +0900

    💄 Add : 날씨 페이지 퍼블리싱

--- App.js
+++ App.js
@@ -1,15 +1,43 @@
-import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={{ flex: 1 }}>
-      <View style={{ flex: 1, backgroundColor: "tomato" }} />{" "}
-      {/* 부모 요소 flex 크기의 1배 */}
-      <View style={{ flex: 2, backgroundColor: "teal" }} />{" "}
-      {/* 부모 요소 flex 크기의 2배 */}
-      <View style={{ flex: 3, backgroundColor: "orange" }} />{" "}
-      {/* 부모 요소 flex 크기의 3배 */}
+    <View style={styles.container}>
+      <View style={styles.city}>
+        <Text style={styles.cityContent}>Seoul</Text>
+      </View>
+      <View style={styles.weather}>
+        <Text style={styles.temperature}>27</Text>
+        <Text style={styles.condition}>Sunny</Text>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "tomato",
+  },
+  city: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  cityContent: {
+    fontSize: "70px",
+  },
+  weather: {
+    flex: 3,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: "-150px",
+  },
+  temperature: {
+    fontSize: "180px",
+  },
+  condition: {
+    fontSize: "55px",
+    marginTop: "-30px",
+  },
+});
